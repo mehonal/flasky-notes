85b8a9af926d0e608344a2572f237cc84c2820e7
Mehonal <116497904+mehonal@users.noreply.github.com>
2023-04-27 14:15:52 +0300

    forced single row for mobile & smaller screen devices on full theme's notes

--- static/script/themes/full/notes.js
+++ static/script/themes/full/notes.js
@@ -7,58 +7,66 @@ function addNewNote(){
 }
 
 function tweakRows(){
-    if (numberOfRows == 1){
+    if (window.innerWidth > 600){
+        if (numberOfRows == 1){
+            noteDivs.forEach((el)=>{
+                el.style.width = "calc(100% - 40px)";
+                el.style.padding = "20px";
+            })
+        }
+        else if (numberOfRows == 2){
+            noteDivs.forEach((el)=>{
+                el.style.width = "calc(50% - 40px)";
+                el.style.padding = "20px";
+            })
+        }
+        else if (numberOfRows == 3){
+            noteDivs.forEach((el)=>{
+                el.style.width = "calc(33.33% - 40px)";
+                el.style.padding = "20px";
+            })
+        }
+        else if (numberOfRows == 4){
+            noteDivs.forEach((el)=>{
+                el.style.width = "calc(25% - 30px)";
+                el.style.padding = "15px";
+            })
+        }
+        else if (numberOfRows == 5){
+            noteDivs.forEach((el)=>{
+                el.style.width = "calc(20% - 30px)";
+                el.style.padding = "15px";
+            })
+        }
+        else if (numberOfRows == 6){
+            noteDivs.forEach((el)=>{
+                el.style.width = "calc(16.666% - 24px)";
+                el.style.padding = "12px";
+            })
+        }
+        else if (numberOfRows == 7){
+            noteDivs.forEach((el)=>{
+                el.style.width = "calc(14.2857% - 20px)";
+                el.style.padding = "10px";
+            })
+        }
+        else if (numberOfRows == 8){
+            noteDivs.forEach((el)=>{
+                el.style.width = "calc(12.5% - 20px)";
+                el.style.padding = "10px";
+            })
+        }
+        else if (numberOfRows == 9){
+            noteDivs.forEach((el)=>{
+                el.style.width = "calc(11.111% - 20px)";
+                el.style.padding = "10px";
+            })
+        }
+    }
+    else{
         noteDivs.forEach((el)=>{
             el.style.width = "calc(100% - 40px)";
             el.style.padding = "20px";
-        })
-    }
-    else if (numberOfRows == 2){
-        noteDivs.forEach((el)=>{
-            el.style.width = "calc(50% - 40px)";
-            el.style.padding = "20px";
-        })
-    }
-    else if (numberOfRows == 3){
-        noteDivs.forEach((el)=>{
-            el.style.width = "calc(33.33% - 40px)";
-            el.style.padding = "20px";
-        })
-    }
-    else if (numberOfRows == 4){
-        noteDivs.forEach((el)=>{
-            el.style.width = "calc(25% - 30px)";
-            el.style.padding = "15px";
-        })
-    }
-    else if (numberOfRows == 5){
-        noteDivs.forEach((el)=>{
-            el.style.width = "calc(20% - 30px)";
-            el.style.padding = "15px";
-        })
-    }
-    else if (numberOfRows == 6){
-        noteDivs.forEach((el)=>{
-            el.style.width = "calc(16.666% - 24px)";
-            el.style.padding = "12px";
-        })
-    }
-    else if (numberOfRows == 7){
-        noteDivs.forEach((el)=>{
-            el.style.width = "calc(14.2857% - 20px)";
-            el.style.padding = "10px";
-        })
-    }
-    else if (numberOfRows == 8){
-        noteDivs.forEach((el)=>{
-            el.style.width = "calc(12.5% - 20px)";
-            el.style.padding = "10px";
-        })
-    }
-    else if (numberOfRows == 9){
-        noteDivs.forEach((el)=>{
-            el.style.width = "calc(11.111% - 20px)";
-            el.style.padding = "10px";
         })
     }
 }
