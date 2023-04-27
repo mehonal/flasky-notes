0b33c766d460f5ce7f878ed774d66968c6917710
Mehonal <116497904+mehonal@users.noreply.github.com>
2023-04-27 23:51:39 +0300

    ensured db exists before running app

--- serve_live.py
+++ serve_live.py
@@ -1,3 +1,5 @@
 from waitress import serve
 import app
+with app.app.app_context():
+    app.db.create_all()
 serve(app.app, host='HOST.PUBLIC.IP.HERE', port=1234)
--- serve_local.py
+++ serve_local.py
@@ -1,2 +1,4 @@
-from app import app
+from app import app, db
+with app.app_context():
+    db.create_all()
 app.run(host="127.0.0.1", port = 5000, debug = True)
